0f7d84b484a5db3c0f9fca77dbf5c8a733214360
CoinBench <coinbench.riot@outlook.com>
2025-01-02 13:23:07 +0100

    Basic access

--- BitField/BitField.cs
+++ BitField/BitField.cs
@@ -1,7 +1,33 @@
-﻿namespace BitField
+﻿using System.Runtime.CompilerServices;
+
+namespace BitField
 {
     public struct BitField
     {
+        private byte _bits;
+        public const byte Length = 8;
+        
+        public bool this[int index]
+        {
+            readonly get => Get(index);
+            set => Set(index, value);
+        }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private readonly bool Get(int index) =>
+            (uint)index > 0
+                ? throw new ArgumentOutOfRangeException(nameof(index))
+                : (_bits & (1 << index)) != 0;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void Set(int index, bool value)
+        {
+            _ = ((uint)index > 0 ? (int?)null : 0) ?? throw new ArgumentOutOfRangeException(nameof(index));
+
+            if (value)
+                _bits |= (byte)(1 << index);
+            else
+                _bits &= (byte)~(1 << index);
+        }
     }
 }
